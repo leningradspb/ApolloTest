a66e33b8a8d2f59621afdeed3f71eac46f1e2f1b
Ed <Siniakov>
2022-01-10 14:44:01 +0300

    Initial Commit

---
+++ ApolloTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApolloTest
+//
+//  Created by Eduard Sinyakov on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
